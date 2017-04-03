04df2e70bad288053f47d768480d081b60920d66
tomalek <tomalek0@o2.pl>
2017-04-03 04:00:50 +0200

    Testow ciag dalszy

--- src/PierwszyProjekt.java
+++ src/PierwszyProjekt.java
@@ -1,11 +1,30 @@
 
-public class PierwszyProjekt {
+public class PierwszyProjekt extends TestDziedziczenie {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-
+		int a = 172;
+		double b = 3.5;
 		System.out.println("siema");
 		System.out.println("asd");
+		System.out.println(Math.random());
+		System.out.println("Wartosc:" + tunczyk());
+		System.out.println("Kwadrat=" + kwadrat(a, b));
+		TestDziedziczenie.dziedziczenie();
+		TestDziedziczenie.main(args);
+		char c;
+		// c = '0';
+		c = (char) a;
+		System.out.println(c);
+	}
+
+	public static int tunczyk() {
+		return 10;
+	}
+
+	public static double kwadrat(double a, double b) {
+		double kwadrat = a * b;
+		return kwadrat;
 	}
 
 }
---
+++ src/TestDziedziczenie.java
@@ -0,0 +1,14 @@
+
+public class TestDziedziczenie {
+	TestDziedziczenie(){
+		System.out.println("ostatni test przed spankiem hehe");
+	}
+	public static void main(String[] args){
+		System.out.println("Tescik jeszcze");
+	}
+	
+	public static void dziedziczenie(){
+		System.out.println("Dziedziczenie dzia³a!");
+	}
+	
+}
